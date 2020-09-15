a6c64a0148dcc76c259f4f58820a5297a359041f
kevin <goldapple820@gmail.com>
2020-09-15 22:24:04 +0300

    Create header slider component

--- src/components/HeaderSlider/HeaderSlider.js
+++ src/components/HeaderSlider/HeaderSlider.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import classes from './HeaderSlider.module.css'
@@ -9,29 +9,41 @@ import 'swiper/swiper-bundle.css'
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
+import './HeaderSliderHelpers.css'
 
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 export default () => {
   return (
-    <Swiper
-      spaceBetween={0}
-      slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      pagination={{ clickable: true }}
-      autoplay
-      loop={true}
-    >
-      <SwiperSlide >
-        <img className={classes.swiper__slide} src="../../../public/images/header-slider/photo1.jpg" alt=""/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className={classes.swiper__slide} src="../../../public/images/header-slider/photo1.jpg" alt=""/>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className={classes.swiper__slide} src="../../../public/images/header-slider/photo1.jpg" alt=""/>
-      </SwiperSlide>
-    </Swiper>
+    <div className={classes.slider}>
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        pagination={{ clickable: true }}
+        autoplay={true}
+        loop={true}
+      >
+        <SwiperSlide >
+          <img className={classes.slider__item} src={require('../../images/header-slider/photo1.jpg')} alt=""/>
+          <p className={classes.slider__text}>Дом Солнце 	&#8211; кооператив. <br/> Мы с любовью изготавливаем <br/> все наши продукты, <br/> не забывая <br/> о здоровье планеты.
+          </p>
+          <img className={classes.slider__logo} src={require('../../images/header-slider/icon.png')} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className={classes.slider__item} src={require('../../images/header-slider/photo2.jpg')} alt=""/>
+          <p className={classes.slider__text}>Дом Солнце 	&#8211; кооператив. <br/> Мы с любовью изготавливаем <br/> все наши продукты, <br/> не забывая <br/> о здоровье планеты.
+          </p>
+          <img className={classes.slider__logo} src={require('../../images/header-slider/icon.png')} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className={classes.slider__item} src={require('../../images/header-slider/photo1.jpg')} alt=""/>
+          <p className={classes.slider__text}>Дом Солнце 	&#8211; кооператив. <br/> Мы с любовью изготавливаем <br/> все наши продукты, <br/> не забывая <br/> о здоровье планеты.
+          </p>
+          <img className={classes.slider__logo} src={require('../../images/header-slider/icon.png')} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
