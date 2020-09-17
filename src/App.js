9601a1178c9e273cf817f4149e715f7c8e9f0ffe
kevin <goldapple820@gmail.com>
2020-09-17 12:00:37 +0300

    Change context delivery

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
-
-
+import MainPageContext from "./pages/MainPage/MainPageContext";
 
 class App extends Component {
+
+  state = {
+    navigationClosed: true,
+  }
+
+  toggleNavigation = () => {
+    this.setState({
+      navigationClosed: !this.state.navigationClosed,
+    })
+  }
+
   render() {
     return (
       <Router>
         <div className="App">
           <Switch>
-            <Route path="/" exact component={MainPage} />
+            <MainPageContext.Provider
+              value={{
+                navigationClosed: this.state.navigationClosed,
+                toggleNavigation: this.toggleNavigation,
+              }}
+            >
+              <Route path="/" exact component={MainPage} />
+            </MainPageContext.Provider>
           </Switch>
         </div>
       </Router>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classes from "./Navigation.css";
+import MainPageContext from '../../pages/MainPage/MainPageContext'
+
 
 export default (props) => {
+  const navContext = useContext(MainPageContext);
+  console.log(navContext.navigationClosed)
   return (
-    <div className={classes.navigation}>
-
-    </div>
-  );
+      <div className={classes.navigation}>{navContext.navigationClosed}</div>
+  )
 };
