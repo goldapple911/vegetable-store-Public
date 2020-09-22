359775db149ce13bd770e5ad4201edf5ee025b1d
kevin <goldapple820@gmail.com>
2020-09-22 23:39:22 +0300

    add buttons styling & layout for

--- src/containers/MainPageContent/MainPageContent.js
+++ src/containers/MainPageContent/MainPageContent.js
@@ -1,29 +1,37 @@
 import React from 'react';
-import classes from './MainPageContent.module.css';
 
+import classes from './MainPageContent.module.css';
 import ScrollButton from '../../components/ScrollButton/ScrollButton';
 import PromoCarusel from '../../components/PromoCarusel/PromoCarusel';
 import LinkButton from '../../components/LinkButton/LinkButton';
 import TeamInfo from '../../components/TeamInfo/TeamInfo';
 import { Link } from 'react-router-dom';
 
-const buttonStyles = {
-  backgroundColor: '#fdcf7d',
-  margin: '10px auto',
-}
-
-const mainPageContent = () => {
+export default () => {
   return (
     <main className={`container ${classes.MainPageContent}`}>
+
+      <p className={classes.text}>Мы поможем вам собрать эко-подарок для самых разных людей! С помощью нескольких фильтров мы вместе составим идеальный набор для конкретного человека</p>
+
       <div className={classes.generator__container}>
-        Заглушка для генератора подарков
+        <LinkButton
+          className={`${classes.link_button} ${classes.link_button__generator}`}
+          title="Собрать подарок"
+          text={'Собрать подарок'}
+        />
       </div>
-      
-      <LinkButton background={'#FDCF7D'} className={classes.link_button} title='Перейти в каталог' text={'Перейти в каталог'} />
+
+      <div className={classes.promocarusel_container}>
+        <PromoCarusel />
+      </div>
+
+      <LinkButton
+        className={classes.link_button}
+        title="Перейти в каталог"
+        text={'Перейти в каталог'} /
+      >
 
 
     </main>
   );
 };
-
-export default mainPageContent;
