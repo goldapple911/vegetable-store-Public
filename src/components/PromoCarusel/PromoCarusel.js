bfcb549e69fb152f3223c05723e8de1bf5adbbca
kevin <goldapple820@gmail.com>
2020-09-22 23:40:23 +0300

    create component promoCarusel

---
+++ src/components/PromoCarusel/PromoCarusel.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default (props) => {
+  const list;
+
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  );
+}
