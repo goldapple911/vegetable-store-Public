488f50e2bf715b795dab73931e3ad710dac177b1
kevin <goldapple820@gmail.com>
2021-01-15 11:17:03 +0300

    Create an Express + GraphQL(Apollo) + React application

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import CartPage from "./CartPage/CartPage";
+import CataloguePage from "./CataloguePage/CataloguePage";
+import MainPage from "./MainPage/MainPage";
+import PolicyPage from "./PolicyPage/PolicyPage";
+import ContactPage from "./ContactPage/ContactPage";
+
+export {CartPage, CataloguePage, MainPage, PolicyPage, ContactPage};
